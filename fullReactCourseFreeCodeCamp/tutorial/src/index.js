2bf773fd123a9bf0ec3ebeab1058117c4ccb6464
aba0405 <adnan.abdal@gmail.com>
2020-10-11 21:28:03 -0400

    Everyday Practice

--- fullReactCourseFreeCodeCamp/tutorial/src/index.js
+++ fullReactCourseFreeCodeCamp/tutorial/src/index.js
@@ -12,15 +12,36 @@ import './index.css'
 // close every element
 // formatting
 
+const firstBook = {
+  img:
+  "https://images-na.ssl-images-amazon.com/images/I/91BQbqTW6oL._AC_UL200_SR200,200_.jpg",
+  title:'Follow the Money: The Shocking Deep State',
+  author:'Dan Bongino'
+}
+
+const secondBook = {
+  img:
+  "https://images-na.ssl-images-amazon.com/images/I/91BQbqTW6oL._AC_UL200_SR200,200_.jpg",
+  title:'Follow the Money: The Shocking Deep State',
+  author:'Dan Bongino'
+}
 
 function BookList (){
   return (
     <section className = 'booklist'>
+      <Book
+      img = {firstBook.img}
+      title = {firstBook.title}
+      author={firstBook.author}
+      >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam beatae nihil non aperiam, odio accusantium porro earum quia! Nesciunt error porro fugiat ratione consequuntur placeat omnis voluptatum sapiente veritatis ducimus.</p>
+      </Book>
       <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
+      <Book
+      img = {secondBook.img}
+      title = {secondBook.title}
+      author={secondBook.author}
+      />
     </section>
   )
 }
@@ -30,22 +51,15 @@ function BookList (){
 //   return <p>This is the Message</p>
 // }
 
-const Book = () => {
-  return <article className = 'book'>
-    <Image></Image>
-    <Title></Title>
-    <Author></Author>
+const Book = ({img, title, author,children}) => {
+  return (
+  <article className = 'book'>
+  <img src={img} alt=""/>
+  <h1>{title}</h1>
+  <h4>{author}</h4>
+  {children}
   </article>
+  )
 }
-
-const Image = () => (
-  <
-    img src="https://images-na.ssl-images-amazon.com/images/I/91BQbqTW6oL._AC_UL200_SR200,200_.jpg" alt=""
-  />
-)
-
-const Title = () => <h1>Follow the Money: The Shocking Deep State</h1>
-
-const Author = () => <h4>Dan Bongino</h4>
 
 ReactDom.render(<BookList/>, document.getElementById('root'))
